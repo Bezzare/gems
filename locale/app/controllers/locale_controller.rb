c4847ba421fb3eb1240bead4839a0289f5927d03
Bezzare <bezzarelin@gmail.com>
2015-07-29 14:00:05 +0800

    locale switch

--- locale/app/controllers/locale_controller.rb
+++ locale/app/controllers/locale_controller.rb
@@ -1,4 +1,15 @@
 class LocaleController < ApplicationController
   def index
   end
+
+  def new
+      # 可以將 ["en", "zh-TW"] 設定為 VALID_LANG 放到 config/environment.rb 中
+      # if params[:locale] && I18n.available_locales.include?( params[:locale].to_sym )
+        session[:locale] = params[:locale]
+      # end
+
+      I18n.locale = session[:locale] || I18n.default_locale
+      redirect_to locale_index_url
+  end
+
 end
